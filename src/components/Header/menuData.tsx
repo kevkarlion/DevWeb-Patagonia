8103782c80095ba2264d500e1b251f40b8e9a72d
Kevin Riquelme <kriquelme10@gmail.com>
2024-10-08 01:30:36 -0300

    start work

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,85 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Servicios",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Proyectos",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Flujo de trabajo",
+    path: "/blog",
+    newTab: false,
+  },
+ 
+  {
+    id: 5,
+    title: "Pages",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "About Page",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Contact Page",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Blog Grid Page",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Blog Sidebar Page",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Blog Details Page",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "Sign In Page",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 47,
+        title: "Sign Up Page",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 48,
+        title: "Error Page",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+];
+export default menuData;
